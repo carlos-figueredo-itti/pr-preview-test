1051f62c03c182775d7b8495045e61268975c987
Carlos Figueredo <carlos.figueredo@itti.digital>
2024-05-28 15:51:43 -0400

    Testing React APP

--- src/App.js
+++ src/App.js
@@ -7,15 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Test de PR Preview
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="//itti.digital"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          DevOps Itti
         </a>
       </header>
     </div>
